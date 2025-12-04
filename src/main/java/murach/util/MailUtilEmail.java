829230854a537967cb83c77ad88663982fe628ab
Quoc Huy <23110103@student.hcmute.edu.vn>
2025-12-04 15:22:28 +0700

    Change SMTP

--- src/main/java/murach/util/MailUtilEmail.java
+++ src/main/java/murach/util/MailUtilEmail.java
@@ -13,13 +13,24 @@ public class MailUtilEmail {
         // 1 - get a mail session
         Properties props = new Properties();
         props.put("mail.transport.protocol", "smtps");
-        props.put("mail.smtps.host", "smtp.gmail.com");
-        props.put("mail.smtps.port", 587);
+        props.put("mail.smtps.host", "sandbox.smtp.mailtrap.io");
+        props.put("mail.smtps.port", "2525");
         props.put("mail.smtps.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true"); // Bắt buộc
         props.put("mail.smtps.quitwait", "false");
-        Session session = Session.getDefaultInstance(props);
-        session.setDebug(true);
+//        Session session = Session.getDefaultInstance(props);
+//        session.setDebug(true);
 
+        String username = "fa27adc8690b25"; 
+        String password = "1544ca9e25aa90";
+        
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+        
+        session.setDebug(true);
         // 2 - create a message
         Message message = new MimeMessage(session);
         message.setSubject(subject);
