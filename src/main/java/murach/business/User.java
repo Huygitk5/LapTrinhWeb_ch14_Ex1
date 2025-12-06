edbce3246ab67860a639cb2c2a6b4b3160009fb8
Quoc Huy <23110103@student.hcmute.edu.vn>
2025-12-06 10:00:08 +0700

    change table's name

--- src/main/java/murach/business/User.java
+++ src/main/java/murach/business/User.java
@@ -5,14 +5,12 @@
 package murach.business;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 
 @Entity
-@Table(name = "USERS")
+@Table(name = "users")
 public class User implements Serializable {
     @Id
     private String email;
